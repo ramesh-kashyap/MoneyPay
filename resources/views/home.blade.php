79bebc3211ea194c8ae4b49dd61660794f907974
sagartyag <sagartyagi1024@gmail.com>
2024-05-01 12:09:26 +0530

    sde

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in  Heloo!') }}
                 </div>
             </div>
         </div>
