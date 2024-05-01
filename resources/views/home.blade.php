ebb70c7f23bc4135b42ccd1bad9d14a3afa87520
sagartyag <sagartyagi1024@gmail.com>
2024-05-01 12:47:01 +0530

    cvf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in  Heloo Sagr!') }}
+                    {{ __('You are logged in  Heloo arpit!') }}
                 </div>
             </div>
         </div>
